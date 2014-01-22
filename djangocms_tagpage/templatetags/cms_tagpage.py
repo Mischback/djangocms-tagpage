805bbfc3a63cdccaa4a122ed1ed635b5390b3a68
Mischback <dev@mischback.de>
2014-01-22 21:26:52 +0100

    Fetch all (used) tags completed

--- djangocms_tagpage/templatetags/cms_tagpage.py
+++ djangocms_tagpage/templatetags/cms_tagpage.py
@@ -4,15 +4,22 @@
 
 # Django imports
 from django import template
+# external app imports
+from taggit.models import Tag, TaggedItem
 
 register = template.Library()
 
-@register.assignment_tag(takes_context=True):
+@register.assignment_tag(takes_context=True)
 def get_tags(context):
     """
     @brief  Fetches Tag objects
     """
     # get all available tags
     tags = Tag.objects.all()
+
+    # just return the tags in use
+    # @todo: Make this configurable?!
+    used_tags = TaggedItem.objects.all().values_list('tag_id', flat=True)
+    tags = tags.filter(id__in=used_tags)
 
     return tags
